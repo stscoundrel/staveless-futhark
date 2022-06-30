4cb06f85ffcc50fb3f3e15608565f3ef7c2a06dd
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-30 11:58:59 +0300

    Add runes to letters transform

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { getLettersToRunesMapping } from './mappings/letters-to-runes';
+import { getRunesToLettersMapping } from './mappings/runes-to-letters';
 import { transform } from './transform';
 
 export const lettersToRunes = (
   content: string,
 ): string => transform(content, getLettersToRunesMapping());
 
+export const runesToLetters = (
+  content: string,
+): string => transform(content, getRunesToLettersMapping());
+
 export default {
   lettersToRunes,
+  runesToLetters,
   getLetterMapping: getLettersToRunesMapping,
+  getRuneMapping: getRunesToLettersMapping,
 };
